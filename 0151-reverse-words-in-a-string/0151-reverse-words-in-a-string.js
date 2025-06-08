486aaac953bc2d19fe99e6a22b89f1903f4e02e8
Lavi Jain <67602004+lavijaindev@users.noreply.github.com>
2025-06-08 22:15:31 +0530

    Time: 4 ms (25.1%), Space: 54.9 MB (63.56%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,7 +3,26 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-        let wordsWithSpace = s.split(" ");
-        let words = wordsWithSpace.filter(word => word.trim() !== '').reverse();
-        return words.join(" ");
+    let i = s.length - 1;
+    let result = [];
+
+    while (i >= 0) {
+        // Skip trailing spaces
+        while (i >= 0 && s[i] === ' ') {
+            i--;
+        }
+        if (i < 0) break;
+
+        let j = i; // End of the word
+
+        // Find the start of the word
+        while (i >= 0 && s[i] !== ' ') {
+            i--;
+        }
+
+        // Extract and add the word
+        result.push(s.slice(i + 1, j + 1));
+    }
+
+    return result.join(' ');
 };
